b201d67c3114ac6d7fa7602488a182a61f5ace94
NickUli <nickuli@vivaldi.net>
2023-06-27 18:00:53 +0300

    create logo

---
+++ component/logo.php
@@ -0,0 +1,10 @@
+<?php
+$logo = '
+    <a class="logo" href="numismatics/index.php">
+        <div class="n">N</div>
+        <div class="u">U</div>
+        <div class="ick">ick</div>
+        <div class="li">li</div>
+        <div class="mis">mis</div>
+        <div class="matics">matics</div>
+    </a>';
